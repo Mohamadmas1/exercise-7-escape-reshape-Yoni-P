450831764453cd9115a046a58aee8db99bb54d91
Yoni-P <116964213+Yoni-P@users.noreply.github.com>
2025-01-26 14:09:33 +0200

    Added adaptive music

--- Assets/Scripts/HintManager.cs
+++ Assets/Scripts/HintManager.cs
@@ -1,18 +1,38 @@
 ﻿using System;
+using FMODUnity;
 using UnityEngine;
 
 public class HintManager : MonoBehaviour
 {
     [SerializeField] private MissionData[] missionData;
+    [SerializeField] private StudioGlobalParameterTrigger progressParameterTrigger;
 
     private float timeForNextHint;
     private float timeSinceLastHint;
     
     private int currentMissionIndex = 0;
+    
+    private float progress = 0;
 
     private void Start()
     {
         timeForNextHint = GameTimer.Instance.GameTime / (missionData.Length + 1);
+        
+        progressParameterTrigger.Value = progress;
+        progressParameterTrigger.TriggerParameters();
+
+        foreach (var missionData in missionData)
+        {
+            missionData.OnMissionSolved += Progress;
+        }
+    }
+    
+    
+    private void Progress()
+    {
+        progress += 1f;
+        progressParameterTrigger.Value = progress;
+        progressParameterTrigger.TriggerParameters();
     }
 
     private void Update()
